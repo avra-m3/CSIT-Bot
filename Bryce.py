7ab678f1f0e31d0210cdb619ce3a95871d58bf8f
Avrami Hammer <avramihammer@gmail.com>
2018-08-12 23:30:15 +1000

    removed auth, added bryce mention interaction

--- Bryce.py
+++ Bryce.py
@@ -1,6 +1,7 @@
 import json
 import os
 import re
+import time
 
 import discord
 import requests
@@ -11,8 +12,13 @@ TEMP_PATH = "./state_{}.temp.json"
 INSTRUCTIONS = """USING THE BRYCE BOT
 COMMANDS:
     !about - You know how to use this one already clearly
-    BRYCE IT UP! - Turn everyone into a better version of themselves
-    KILL THE LIGHTS - AND flip it back
+    Requires the user be an administrator
+        <insert text> it up! - change the name of all members to <insert text>
+        !restore <args> - restore usernames
+            default behaviour: restores from an internal state if one exists.
+            from <url>: fetches the file from url and attempts to restore state from said file.
+            from <attachment>: fetches the attachment and attempts to restore state from said file.
+    Mention 'bryce' to check if the bot is online
 """
 
 
@@ -23,31 +29,28 @@ async def log_state(server, channel):
     :param server: The discord server object
     :return: A state mapping or None if it was not safe to create a new state
     """
-    state = {}
+    if os.path.isfile(TEMP_PATH.format(server.id)):
+        with open(TEMP_PATH.format(server.id), "r") as state_in:
+            state = json.load(state_in)
+    else:
+        state = {}
     for member in server.members:
-        if member.top_role < server.me.top_role:
-            state[member.id] = member.nick
-    if os.path.isfile(TEMP_PATH.format(server.id)) or not state:
+        if member.id not in state:
+            if member.top_role < server.me.top_role:
+                state[member.id] = member.nick
+    if not state:
         return None
     with open(TEMP_PATH.format(server.id), "w+") as output:
         json.dump(state, output)
     with open(TEMP_PATH.format(server.id), "r") as output:
         await client.send_file(channel, output)
-    print(state)
     return state
 
 
-# def reset():
-
-
-async def set_bryce_state(server):
-    for member in server.members:
-        if member.top_role < server.me.top_role:
-            try:
-                print("SET {} > {}".format(member.nick, "Bryce"))
-                await client.change_nickname(member, "Bryce")
-            except Exception:
-                pass
+def archive(file_path):
+    now = time.strftime("%y%m%d-%H%M%S")
+    archive_path = "{p}_{time}.archive".format(p=file_path, time=now)
+    os.rename(file_path, archive_path)
 
 
 async def reset_state_from_local(server):
@@ -63,7 +66,7 @@ async def reset_state_from_local(server):
         state = json.load(state_in)
 
     await commit_state(state, server)
-    os.remove(TEMP_PATH.format(server.id))
+    archive(TEMP_PATH.format(server.id))
     return True
 
 
@@ -81,7 +84,7 @@ async def reset_state_from_remote(server, url):
         return False
     await commit_state(state, server)
     if os.path.exists(TEMP_PATH.format(server.id)):
-        os.remove(TEMP_PATH.format(server.id))
+        archive(TEMP_PATH.format(server.id))
     return True
 
 
@@ -118,54 +121,67 @@ async def on_ready():
             print("%-30s%-20s" % (server, "Good"))
 
 
+
 @client.event
 async def on_message(message):
     server = message.server
     command = message.content.lower().strip()
     if command.startswith("!about"):
         await client.send_message(message.channel, INSTRUCTIONS)
-    if command.endswith(' it up!'):
-        await client.send_message(message.channel, 'ACK -> Executing subroutine \'Good Cop\'')
-        state = await log_state(server, message.channel)
-        if state is not None:
-            for key in state:
-                state[key] = message.content.strip()[:-7]
-            await commit_state(state, server)
-            await client.send_message(message.channel, 'Execution Complete')
+    if message.author.server_permissions.administrator:
+        if command.endswith(' it up!'):
+            await client.send_message(message.channel, 'ACK -> Executing subroutine \'Good Cop\'')
+            new_nick = message.clean_content.strip()[:-7]
+            if len(new_nick) < 32:
+                state = await log_state(server, message.channel)
+                if state is not None:
+                    for key in state:
+                        state[key] = new_nick
+                    await commit_state(state, server)
+                    await client.send_message(message.channel, 'Execution Complete')
 
-        else:
-            await client.send_message(message.channel, 'Execution failed; no targets found or state already exists')
-    if command.startswith("!restore"):
-        if command == "!restore":
-            await client.send_message(message.channel, 'ACK -> Executing subroutine \'Bad Cop\'')
-            if not await reset_state_from_local(server):
-                await client.send_message(message.channel, 'State reset failed; Reason, no current state available')
+                else:
+                    await client.send_message(message.channel, 'Execution failed; no targets found or state already exists')
+            else:
+                await client.send_message(message.channel,
+                                          'Cannot change everyone\'s nickname to {} as len > 32'.format(new_nick))
 
-        if command.startswith("!restore from"):
-            command_end = message.content.strip("!restore from").strip()
-            regex = re.compile(
-                r'^(?:http|ftp)s?://'  # http:// or https://
-                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-                r'localhost|'  # localhost...
-                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-                r'(?::\d+)?'  # optional port
-                r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-            if re.match(regex, command_end):
-                await client.send_message(message.channel, "ACK -> Attempting restore from {}".format(command_end))
-                if not await reset_state_from_remote(server, command_end):
-                    await client.send_message(message.channel, "OP failed; Bad file")
+        if command.startswith("!restore"):
+            if command == "!restore":
+                await client.send_message(message.channel, 'ACK -> Executing subroutine \'Bad Cop\'')
+                if not await reset_state_from_local(server):
+                    await client.send_message(message.channel, 'State reset failed; Reason, no current state available')
 
-            elif message.attachments:
-                for attachment in message.attachments:
-                    await client.send_message(message.channel,
-                                              "ACK -> Attempting restore from attachment at {}".format(attachment.url))
-                    if not await reset_state_from_remote(server, attachment):
+            if command.startswith("!restore from"):
+                command_end = message.content.strip("!restore from").strip()
+                regex = re.compile(
+                    r'^(?:http|ftp)s?://'  # http:// or https://
+                    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+                    r'localhost|'  # localhost...
+                    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+                    r'(?::\d+)?'  # optional port
+                    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+                if re.match(regex, command_end):
+                    await client.send_message(message.channel, "ACK -> Attempting restore from {}".format(command_end))
+                    if not await reset_state_from_remote(server, command_end):
                         await client.send_message(message.channel, "OP failed; Bad file")
+                elif message.attachments:
+                    for attachment in message.attachments:
+                        await client.send_message(message.channel,
+                                                  "ACK -> Attempting restore from attachment at {}".format(attachment.url))
+                        if not await reset_state_from_remote(server, attachment):
+                            await client.send_message(message.channel, "OP failed; Bad file")
+                else:
+                    await client.send_message(message.channel, "OP failed; Bad file")
     if "bryce" in command:
         if message.author != server.me:
             await client.send_message(message.channel, "I love Bryce, don't you?")
             await client.add_reaction(message, "❤")
-
+    if "token" in command or "auth" in command:
+        if message.author != server.me:
+            await client.send_message(message.channel, "Hide your tokens kiddos!")
+            await client.add_reaction(message, "🐱")
+            await client.add_reaction(message, "💻")
 
 try:
     client.run(os.getenv('AUTH'))
